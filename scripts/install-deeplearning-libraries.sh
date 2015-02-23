39b8dbdd08c1bd5c6ea7b18541efc5f60dbc5633
Olivier Grisel <olivier.grisel@ensta.org>
2015-02-23 21:31:30 +0000

    Correct regristration of circus in upstart

--- scripts/install-deeplearning-libraries.sh
+++ scripts/install-deeplearning-libraries.sh
@@ -104,5 +104,6 @@ fi
 # Register the circus daemon with Upstart
 if [ ! -f "/etc/init/circus.conf" ]; then
     sudo ln -s /home/ubuntu/dl-machine/circus.conf /etc/init/circus.conf
+    sudo initctl reload-configuration
 fi
 sudo service circus restart
